90390a247c18a49ee2b3cd83737940801b771c3e
Anneli Olsson <anneli.ohlsson@gmail.com>
2024-01-03 13:45:17 +0100

    changed file names

--- anneli_olsson.js
+++ anneli_olsson.js
@@ -1,25 +1,23 @@
 
-      const form = document.getElementById('quiz');
-      form.addEventListener('submit', function (event) {validate( form, event )
-   })
+    let form = document.getElementById('quiz');
+    form.addEventListener('submit', validate);
 
-   
-   function validate (form, event ) {
+   function validate (event) {
     let valueFirstName = document.getElementById('firstname').value;
     let errorFirstName = document.getElementById('errorFirstName');
     let letters = /^[A-Za-z]+$/;
         
-        if(valueFirstName === '' || (!isNaN(valueFirstName)) || !valueFirstName.match(letters)) {
-        errorFirstName.innerText ='Please enter your first name (only letters)';
-        errorFirstName.style.color = 'red';
+    if(valueFirstName === '' || (!isNaN(valueFirstName)) || !valueFirstName.match(letters)) {
+    errorFirstName.innerText ='Please enter your first name (only letters)';
+    errorFirstName.style.color = 'red';
     }
 
     else {
         errorFirstName.innerText ='';
     }
 
-    valueLn = document.getElementById('lastname').value;
-    errorLn = document.getElementById('errorLastName');
+    let valueLn = document.getElementById('lastname').value;
+    let errorLn = document.getElementById('errorLastName');
     if(valueLn === '' || (!isNaN(valueLn)) || !valueLn.match(letters)) {
         errorLn.innerText = 'Please enter your last name (only letters)';
         errorLn.style.color = 'red';
@@ -29,8 +27,8 @@
 
     }
 
-    valueEmail = document.getElementById('email').value;
-    errorEmail = document.getElementById('errorEmail');
+    let valueEmail = document.getElementById('email').value;
+    let errorEmail = document.getElementById('errorEmail');
     if( valueEmail === '' || ( valueEmail.indexOf('@') === -1) || (valueEmail.indexOf('.') === -1 ) ) {
         errorEmail.innerText = 'Please enter a valid email adress';
         errorEmail.style.color = 'red';
@@ -39,22 +37,19 @@
         errorEmail.innerText ='';
     }
 
-    errorfrontEnd = document.getElementById('errorfrontEnd');
-    
-        let checkedCss = document.getElementById('frontEndCss').checked;
-        let checkedCplusPlus = document.getElementById('frontEndc++').checked;
-        let checkedJavaScript = document.getElementById('frontEndJs').checked;
+    let errorfrontEnd = document.getElementById('errorfrontEnd');
+    let checkedCss = document.getElementById('frontEndCss').checked;
+    let checkedCplusPlus = document.getElementById('frontEndc++').checked;
+    let checkedJavaScript = document.getElementById('frontEndJs').checked;
 
-        if(checkedCss === false && checkedCplusPlus === false && checkedJavaScript === false) {
-            errorfrontEnd.innerText = 'Please select an option';
-            errorfrontEnd.style.color = 'red';
+    if(checkedCss === false && checkedCplusPlus === false && checkedJavaScript === false) {
+        errorfrontEnd.innerText = 'Please select an option';
+        errorfrontEnd.style.color = 'red';
                     }
-        else {
-            errorfrontEnd.innerText ='';
+    else {errorfrontEnd.innerText ='';
     }
 
-    errorFruit = document.getElementById('errorFruit');
-
+    let errorFruit = document.getElementById('errorFruit');
     let checkedBlueSunset = document.getElementById('sunsetMarsBlue').checked;
     let checkedArmpit = document.getElementById('sweatArmpit').checked;
     let checkedApple = document.getElementById('fruitApple').checked;
@@ -62,7 +57,7 @@
     let checkedStrawBerry = document.getElementById('fruitStrawberry').checked;
     let checkedKiwi = document.getElementById('fruitKiwi').checked;
     
-    if(checkedApple === false && checkedAubergine === false && checkedStrawBerry === false && checkedKiwi === false){
+    if(checkedApple === false && checkedAubergine === false && checkedStrawBerry === false && checkedKiwi === false) {
         errorFruit.innerText = 'Please select at least one option';
         errorFruit.style.color = 'red';
     }
@@ -78,49 +73,55 @@
 
     if(validateFirstName === true && validateLn === true && validateEmail === true && validatefrontEnd === true && validateFruit === true) {
         alert('The quiz was succussfully filled out!');
-        }
+        let showAnswers = document.getElementById('showAnswers');
+        let showAnswersHeader = document.getElementById('showAnswersHeader');
     
-    let score = 0
-    let showScore = document.getElementById('showScore')
+        showAnswersHeader.innerText = 'The correct answers are:';
+        showAnswers.innerText = 'Blue\nThe armpit\nC++\nApple, strawberry, kiwi';
+
+    let score = 0;
+    let showScore = document.getElementById('showScore');
 
     if(validateFirstName === true && validateLn === true && validateEmail === true && validatefrontEnd === true && validateFruit === true && checkedBlueSunset === true) {
         score = score + 1
         showScore.innerText = 'Your result is ' + score + '/4';
-        showScore.style.backgroundColor = 'yellow'
+        showScore.style.backgroundColor = 'yellow';
     }
-
+    
     if(validateFirstName === true && validateLn === true && validateEmail === true && validatefrontEnd === true && validateFruit === true && checkedArmpit === true) {
         score = score + 1
         showScore.innerText = 'Your result is ' + score + '/4';
-        showScore.style.backgroundColor = 'yellow'
+        showScore.style.backgroundColor = 'yellow';
     }
 
     if(validateFirstName === true && validateLn === true && validateEmail === true && validatefrontEnd === true && validateFruit === true && checkedCplusPlus === true) {
         score = score + 1
         showScore.innerText = 'Your result is ' + score + '/4';
-        showScore.style.backgroundColor = 'yellow'
+        showScore.style.backgroundColor = 'yellow';
     }
           
     if(validateFirstName === true && validateLn === true && validateEmail === true && validatefrontEnd === true && validateFruit === true && checkedStrawBerry === true && checkedApple === true && checkedKiwi === true && checkedAubergine === false) {
         score = score + 1
         showScore.innerText = 'Your result is ' + score + '/4';
-        showScore.style.backgroundColor = 'yellow'
+        showScore.style.backgroundColor = 'yellow';
     }
 
     if(validateFirstName === true && validateLn === true && validateEmail === true && validatefrontEnd === true && validateFruit === true && score < 1) {
         showScore.innerText = 'Your result is 0/4';
-        showScore.style.backgroundColor = 'yellow'  
+        showScore.style.backgroundColor = 'yellow';  
     }
-    score = 0
-
-    let showAnswers = document.getElementById('showAnswers')
-    let showAnswersHeader = document.getElementById('showAnswersHeader')
-    showAnswersHeader.innerText = 'The correct answers are:'
-    showAnswers.innerText = 'Blue\nThe armpit\nC++\nApple, strawberry, kiwi'
     
- 
+    score = 0;
+    }
+
+    else {showAnswersHeader.innerText = '';
+    showAnswers.innerText = ''; 
+    showScore.innerText = ''; 
+}
+
+
     event.preventDefault();
-    return
+    return;
    }
 
    
